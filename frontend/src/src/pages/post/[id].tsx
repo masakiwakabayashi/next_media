39f5254725362949eec5c19621b8050aeca864e3
若林将輝 <masaki.wakabaya@gmail.com>
2024-12-22 01:24:03 +0900

    install tailwind

---
+++ frontend/src/src/pages/post/[id].tsx
@@ -0,0 +1,33 @@
+
+
+const index = () => {
+  return (
+    <div>
+    <div className="flex gap-4 mx-4">
+      <div className="w-1/2 flex flex-col gap-1">
+        <div className="bg-green-400">Column Left Row 1</div>
+        <div className="bg-green-400">Column Left Row 2</div>
+        <div className="bg-green-400">Column Left Row 3</div>
+      </div>
+      <div className="w-1/2  flex flex-col gap-1">
+        <div className="bg-blue-500">Column Right Row 1</div>
+        <div className="bg-blue-500">Column Right Row 2</div>
+        <div className="bg-blue-500">Column Right Row 3</div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+    </div>
+  );
+}
+
+export default index;
+
+
