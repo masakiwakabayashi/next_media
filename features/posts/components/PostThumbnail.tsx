5ca87ab7198bbe345383c7e700bb78f9b8433f19
masakiwakabayashi <masaki.wakabaya@gmail.com>
2026-03-21 16:42:32 +0900

    ディレクトリ構成を変更

--- features/posts/components/PostThumbnail.tsx
+++ features/posts/components/PostThumbnail.tsx
@@ -4,12 +4,12 @@ import Image from 'next/image'
 import { useState } from 'react'
 
 type Props = {
-  src: string
+  src: string | null
   alt: string
 }
 
 export default function PostThumbnail({ src, alt }: Props) {
-  const [imgSrc, setImgSrc] = useState(src)
+  const [imgSrc, setImgSrc] = useState(src ?? '/no_image.png')
 
   return (
     <div className="relative mb-4 aspect-video w-full overflow-hidden rounded-lg">
