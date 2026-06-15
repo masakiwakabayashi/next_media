bf11d9e6dc4d0650bdc47ac3d4befeec215f7dde
masakiwakabayashi <masaki.wakabaya@gmail.com>
2026-06-15 21:26:07 +0900

    設定ファイルなどを修正

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next'
+import { Suspense } from 'react'
 import LoginForm from '@/features/auth/components/LoginForm'
 
 export const metadata: Metadata = {
@@ -8,7 +9,9 @@ export const metadata: Metadata = {
 export default function LoginPage() {
   return (
     <div className="w-full">
-      <LoginForm />
+      <Suspense>
+        <LoginForm />
+      </Suspense>
     </div>
   )
 }
